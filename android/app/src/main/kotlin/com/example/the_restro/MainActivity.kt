8f6bab88be046b4ffe497d3d5a95e2333ab0c378
ketan_ambekar <ketan.ambekar@finoux.com>
2026-06-24 15:31:32 +0530

    Initial Setup + Adding Color Constants + Fonts + Getx Packages

---
+++ android/app/src/main/kotlin/com/example/the_restro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.the_restro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:the_restro/app/app.dart';
+
+void main() {
+  runApp(const RestroApp());
+}
